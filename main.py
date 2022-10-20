59a280576c24935bcfc240b1a752921d9fabc2f1
ArtemKAF <grot_artem@inbox.ru>
2022-10-20 03:39:45 +0300

    Add count sorting

--- main.py
+++ main.py
@@ -2,12 +2,20 @@
 import time
 from datetime import timedelta
 import bubblesort as bs
+import countsort as cs
 
 if __name__ == '__main__':
     ns_list = [5, 3, 233, 9, 300, 1, 5, 143, 3, 27, 99, 1, 15, 5, 77, 1, 246, 11, 9, 15, 99, 194, 2, 111, 0,
-               13, 298, 7, 19, -5, 6, 54, 34, -1, 62, 286, 39, 174, -15, -100, 59, 37, 82, 61, 90, -33, -64]
+               13, 298, 7, 19, 5, 6, 54, 34, 1, 62, 286, 39, 174, 15, 100, 59, 37, 82, 61, 90, 33, 64]
+
     s_time = time.monotonic()
     s_list = bs.bubblesort(ns_list)
     e_time = time.monotonic()
     print(s_list)
     print(f"Время сортировки массива сортировкой пузырьком: {timedelta(e_time - s_time)}")
+
+    s_time = time.monotonic()
+    s_list = cs.count_sort(ns_list)
+    e_time = time.monotonic()
+    print(s_list)
+    print(f"Время сортировки массива сортировкой подсчётом: {timedelta(e_time - s_time)}")
